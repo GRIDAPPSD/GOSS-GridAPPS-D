6a9b320fb7840655b071bf961f51c0bb514c9fe9
Craig Allwardt <craig.allwardt@pnnl.gov>
2017-02-27 10:10:20 -0800

    Prepared for osgi.enroute.dto.api no methods on dtos because they are pure data. Serialization to/from dto will happen in a service.

--- pnnl.goss.gridappsd/src/pnnl/goss/gridapsd/dto/PowerSystemConfig.java
+++ pnnl.goss.gridappsd/src/pnnl/goss/gridapsd/dto/PowerSystemConfig.java
@@ -1,41 +1,17 @@
-package pnnl.gos.gridapsd.dto;
+package pnnl.goss.gridapsd.dto;
 
 // TODO change to be a dto rather than full implementation of getters and setters.
 public class PowerSystemConfig {
-	private String SubGeographicalRegion_name;
+	public static String SubGeographicalRegion_name;
 
-	private String GeographicalRegion_name;
+	public static String GeographicalRegion_name;
 
-	private String Line_name;
+	public static String Line_name;
 
-	public String getSubGeographicalRegion_name() {
-		return SubGeographicalRegion_name;
-	}
-
-	public void setSubGeographicalRegion_name(String SubGeographicalRegion_name) {
-		this.SubGeographicalRegion_name = SubGeographicalRegion_name;
-	}
-
-	public String getGeographicalRegion_name() {
-		return GeographicalRegion_name;
-	}
-
-	public void setGeographicalRegion_name(String GeographicalRegion_name) {
-		this.GeographicalRegion_name = GeographicalRegion_name;
-	}
-
-	public String getLine_name() {
-		return Line_name;
-	}
-
-	public void setLine_name(String Line_name) {
-		this.Line_name = Line_name;
-	}
-
-	@Override
-	public String toString() {
-		return "ClassPojo [SubGeographicalRegion_name = "
-				+ SubGeographicalRegion_name + ", GeographicalRegion_name = "
-				+ GeographicalRegion_name + ", Line_name = " + Line_name + "]";
-	}
+//	@Override
+//	public String toString() {
+//		return "ClassPojo [SubGeographicalRegion_name = "
+//				+ SubGeographicalRegion_name + ", GeographicalRegion_name = "
+//				+ GeographicalRegion_name + ", Line_name = " + Line_name + "]";
+//	}
 }
