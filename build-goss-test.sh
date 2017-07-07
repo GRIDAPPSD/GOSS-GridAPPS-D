2e81cfdc79616b7f50edf02bc78c62f886c41265
Gibson, Tara D <tara@pnnl.gov>
2017-07-07 16:09:32 -0700

    no message

--- build-goss-test.sh
+++ build-goss-test.sh
@@ -3,8 +3,8 @@ gradle export
 #copy built jar to dist directory
 cp generated/distributions/executable/run.bnd.jar ~/gridappsd_project/builds/lib/
 
-cp gov.pnnl.goss.gridappsd/applications/python/fncs_goss_bridge.py ~/gridappsd_project/builds/scripts/
-cp gov.pnnl.goss.gridappsd/applications/etc/zipload_schedule.player ~/gridappsd_project/builds/scripts/
+cp applications/python/fncs_goss_bridge.py ~/gridappsd_project/builds/scripts/
+cp applications/etc/zipload_schedule.player ~/gridappsd_project/builds/scripts/
 
 cp ~/gridappsd_project/sources/fncs/python/fncs.py ~/gridappsd_project/builds/scripts/
 cp -R ~/gridappsd_project/sources/GOSS-GridAPPS-D/applications ~/gridappsd_project/builds/
