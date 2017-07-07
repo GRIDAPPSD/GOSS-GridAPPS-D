4c382e940b9cd332a92588c61356703d5828a857
Gibson, Tara D <tara@pnnl.gov>
2017-07-07 16:06:19 -0700

    Updates to build and run scripts

--- run-goss-test.sh
+++ run-goss-test.sh
@@ -2,7 +2,23 @@ export GRIDAPPSD_PROJECT=/home/gridappsd/gridappsd_project
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GRIDAPPSD_PROJECT/builds/lib:/usr/local/lib
 export FNCS_LOG_LEVEL="DEBUG4"
 
+cd $GRIDAPPSD_PROJECT/sources/viz/
+node server.js >> $GRIDAPPSD_PROJECT/builds/log/nodeserver.log 2>&1 &
+NJSPID=`echo $!`
+
 cd $GRIDAPPSD_PROJECT/builds/lib/
-java -Dfelix.cm.home=$GRIDPPAD_PROJECT/builds/test/conf/ -Dfelix.system.properties=$GRIDAPPSD_PROJECT/builds/conf/config.properties -jar run.bnd.jar >> $GRIDAPPSD_PROJECT/builds/log/goss-gridappsd.log 2>&1
+
+java -jar blazegraph.jar >> $GRIDAPPSD_PROJECT/builds/log/blazegraph.log 2>&1 &
+BGPID=`echo $!`
+
+
+java -Dfelix.cm.home=$GRIDAPPSD_PROJECT/builds/conf/ -Dfelix.system.properties=$GRIDAPPSD_PROJECT/builds/conf/config.properties -jar run.bnd.jar >> $GRIDAPPSD_PROJECT/builds/log/goss-gridappsd.log 2>&1
+
+
+echo "Stopping blazegraph, process $BGPID"
+kill $BGPID
+
+echo "Stopping node server, process $NJSPID"
+kill $NJSPID
 
 exit(0)
