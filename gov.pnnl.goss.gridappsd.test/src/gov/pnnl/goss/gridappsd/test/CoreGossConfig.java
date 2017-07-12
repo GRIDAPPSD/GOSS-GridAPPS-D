585e5270d8a21164bf3660c2f9ae18622aacbf5d
Craig Allwardt <craig.allwardt@pnnl.gov>
2017-07-11 21:39:04 -0700

    Added integration tests with the second project.

---
+++ gov.pnnl.goss.gridappsd.test/src/gov/pnnl/goss/gridappsd/test/CoreGossConfig.java
@@ -0,0 +1,57 @@
+package gov.pnnl.goss.gridappsd.test;
+import org.amdatu.testing.configurator.ConfigurationSteps;
+import static org.amdatu.testing.configurator.TestConfigurator.createConfiguration;
+
+import pnnl.goss.core.ClientFactory;
+
+/**
+ * Standard configuration that is required for us to use goss in integration tests.
+ * 
+ * These configuration steps can be used as a guide to building cfg files
+ * for the bundles.
+ * 
+ * @author Craig Allwardt
+ *
+ */
+public class CoreGossConfig {
+	
+	/**
+	 * Minimal configuration for goss including broker uri
+	 * @return
+	 */
+	public static ConfigurationSteps configureServerAndClientPropertiesConfig(){
+		
+		return ConfigurationSteps.create()
+				.add(createConfiguration("pnnl.goss.core.server")
+					.set("goss.openwire.uri", "tcp://localhost:6000")
+					.set("goss.stomp.uri",  "stomp://localhost:6001") //vm:(broker:(tcp://localhost:6001)?persistent=false)?marshal=false")
+					.set("goss.ws.uri", "ws://localhost:6002")
+					.set("goss.start.broker", "true")
+					.set("goss.broker.uri", "tcp://localhost:6000"))
+				.add(createConfiguration(ClientFactory.CONFIG_PID)
+					.set("goss.openwire.uri", "tcp://localhost:6000")
+					.set("goss.stomp.uri",  "stomp://localhost:6001")
+					.set("goss.ws.uri", "ws://localhost:6002"))
+				.add(createConfiguration("org.ops4j.pax.logging")
+					.set("log4j.rootLogger", "DEBUG, out, osgi:*")
+					.set("log4j.throwableRenderer", "org.apache.log4j.OsgiThrowableRenderer")
+
+					//# CONSOLE appender not used by default
+					.set("log4j.appender.stdout", "org.apache.log4j.ConsoleAppender")
+					.set("log4j.appender.stdout.layout", "org.apache.log4j.PatternLayout")
+					.set("log4j.appender.stdout.layout.ConversionPattern", "%-5.5p| %c{1} (%L) | %m%n")
+					//#server.core.internal.GossRequestHandlerRegistrationImpl", "DEBUG,stdout
+					.set("log4j.logger.pnnl.goss", "DEBUG, stdout")
+					.set("log4j.logger.org.apache.aries", "INFO")
+
+					//# File appender
+					.set("log4j.appender.out", "org.apache.log4j.RollingFileAppender")
+					.set("log4j.appender.out.layout", "org.apache.log4j.PatternLayout")
+					.set("log4j.appender.out.layout.ConversionPattern", "%d{ISO8601} | %-5.5p | %-16.16t | %-32.32c{1} | %X{bundle.id} - %X{bundle.name} - %X{bundle.version} | %m%n")
+					.set("log4j.appender.out.file", "felix.log")
+					.set("log4j.appender.out.append", "true")
+					.set("log4j.appender.out.maxFileSize", "1MB")
+					.set("log4j.appender.out.maxBackupIndex", "10"));
+		
+	}
+}
---
+++ gov.pnnl.goss.gridappsd.test/src/gov/pnnl/goss/gridappsd/test/TestConfigurationManager.java
@@ -0,0 +1,95 @@
+package gov.pnnl.goss.gridappsd.test;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import org.amdatu.testing.configurator.TestConfiguration;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.util.tracker.ServiceTracker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.amdatu.testing.configurator.TestConfigurator.configure;
+import static org.amdatu.testing.configurator.TestConfigurator.cleanUp;
+import static org.amdatu.testing.configurator.TestConfigurator.createServiceDependency;
+import static org.junit.Assert.assertNotNull;
+
+import gov.pnnl.goss.gridappsd.api.ConfigurationManager;
+import gov.pnnl.goss.gridappsd.dto.RequestSimulation;
+import pnnl.goss.core.ClientFactory;
+import pnnl.goss.core.server.ServerControl;
+
+/**
+ * 
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestConfigurationManager {
+
+	private static Logger log = LoggerFactory.getLogger(TestConfigurationManager.class);
+   // private final BundleContext context = FrameworkUtil.getBundle(this.getClass()).getBundleContext();
+    
+	private TestConfiguration testConfig;
+	private volatile ClientFactory clientFactory;
+	private volatile ServerControl serverControl;
+	
+	
+	private static final String OPENWIRE_CLIENT_CONNECTION = "tcp://localhost:6000";
+	private static final String STOMP_CLIENT_CONNECTION = "stomp://localhost:6000";
+
+	@Before
+	public void before() throws InterruptedException{	
+		testConfig = configure(this)
+						.add(CoreGossConfig.configureServerAndClientPropertiesConfig())
+						.add(createServiceDependency().setService(ClientFactory.class))
+						.add(createServiceDependency().setService(Logger.class))
+						.add(createServiceDependency().setService(SecurityManager.class))
+						.add(createServiceDependency().setService(ServerControl.class));
+		testConfig.apply();
+		
+		// Configuration update is asyncronous, so give a bit of time to catch up
+		TimeUnit.MILLISECONDS.sleep(1000);
+	}
+	
+	@Test
+	public void sanity_ServerStarted() {
+		log.debug("TEST: serverCanStartSuccessfully");
+		System.out.println("TEST: serverCanStartSuccessfully");
+		assertNotNull(serverControl);
+		log.debug("TEST_END: serverCanStartSuccessfully");
+	}
+	
+    
+    /*
+     * File getSimulationFile(int simulationId, RequestSimulation powerSystemConfig) throws Exception;
+	String getConfigurationProperty(String key);
+     */
+    
+    @Test
+    public void testGetConfigurationProperty(){
+    	//ConfigurationManager manager = getService(ConfigurationManager.class);
+    	
+    	//manager.getConfigurationProperty(key)
+    }
+    
+    
+    
+//    <T> T getService(Class<T> clazz) throws InterruptedException {
+//    	ServiceTracker<T,T> st = new ServiceTracker<>(context, clazz, null);
+//    	st.open();
+//    	return st.waitForService(1000);
+//    }
+    
+    @After
+    public void after() {
+      cleanUp(this);
+    }
+}
+
