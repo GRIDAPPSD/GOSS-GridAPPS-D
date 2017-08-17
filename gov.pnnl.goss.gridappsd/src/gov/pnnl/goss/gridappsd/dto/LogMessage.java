807fd6970bce6bca83b90b8e73e053b962e76ef8
poorvasharma <poorva1209@gmail.com>
2017-08-17 11:54:58 -0700

    moved log to gitignore under folder

---
+++ gov.pnnl.goss.gridappsd/src/gov/pnnl/goss/gridappsd/dto/LogMessage.java
@@ -0,0 +1,42 @@
+package gov.pnnl.goss.gridappsd.dto;
+
+public class LogMessage {
+	
+	String process_id;
+	String timestamp;
+	String log_message;
+	String log_level;
+	String process_status;
+	
+	public String getProcess_id() {
+		return process_id;
+	}
+	public void setProcess_id(String process_id) {
+		this.process_id = process_id;
+	}
+	public String getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getLog_message() {
+		return log_message;
+	}
+	public void setLog_message(String log_message) {
+		this.log_message = log_message;
+	}
+	public String getLog_level() {
+		return log_level;
+	}
+	public void setLog_level(String log_level) {
+		this.log_level = log_level;
+	}
+	public String getProcess_status() {
+		return process_status;
+	}
+	public void setProcess_status(String process_status) {
+		this.process_status = process_status;
+	}
+	
+}
