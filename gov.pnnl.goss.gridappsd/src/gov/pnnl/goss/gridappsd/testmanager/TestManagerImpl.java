f5da6741f831a95a1610d709bdd4a814469fdcf5
poorvasharma <poorva1209@gmail.com>
2018-01-31 14:37:32 -0800

    updated test manager to add source to log messages

--- gov.pnnl.goss.gridappsd/src/gov/pnnl/goss/gridappsd/testmanager/TestManagerImpl.java
+++ gov.pnnl.goss.gridappsd/src/gov/pnnl/goss/gridappsd/testmanager/TestManagerImpl.java
@@ -201,6 +201,7 @@ public class TestManagerImpl implements TestManager {
 	
 	private LogMessage createLogMessage() {
 		LogMessage logMessageObj = new LogMessage();
+		logMessageObj.setSource(this.getClass().getSimpleName());
 		logMessageObj.setLogLevel(LogLevel.DEBUG);
 		logMessageObj.setSource(this.getClass().getSimpleName());
 		logMessageObj.setProcessStatus(ProcessStatus.RUNNING);
