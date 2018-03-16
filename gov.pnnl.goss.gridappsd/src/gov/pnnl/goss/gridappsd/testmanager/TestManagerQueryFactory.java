51e6001d7e6ee39a025de4c8e388b45056811c65
Jeff Simpson <Jeff.Simpson@nrel.gov>
2018-03-16 10:00:45 -0600

    Updated TestManagerQueryFactory constructer interface

--- gov.pnnl.goss.gridappsd/src/gov/pnnl/goss/gridappsd/testmanager/TestManagerQueryFactory.java
+++ gov.pnnl.goss.gridappsd/src/gov/pnnl/goss/gridappsd/testmanager/TestManagerQueryFactory.java
@@ -5,9 +5,12 @@ import org.apache.felix.dm.annotation.api.ServiceDependency;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
 
+import gov.pnnl.goss.cim2glm.components.DistCapacitor;
+import gov.pnnl.goss.cim2glm.components.DistCoordinates;
 import gov.pnnl.goss.cim2glm.queryhandler.QueryHandler;
 import gov.pnnl.goss.gridappsd.api.ConfigurationManager;
 import gov.pnnl.goss.gridappsd.data.handlers.BlazegraphQueryHandler;
+import gov.pnnl.goss.gridappsd.utils.GridAppsDConstants;
 
 @Component
 public class TestManagerQueryFactory {
@@ -21,7 +24,14 @@ public class TestManagerQueryFactory {
 	
 
 	public String getFeeder() {
+		
+		
 		QueryHandler queryHandler = getQueryHandler();
+		
+//		ResultSet results1 = queryHandler.query (DistCapacitor.szQUERY);
+
+//		ResultSet results2 = queryHandler.query (DistCoordinates.szQUERY);
+		
 		String lineQuery = "SELECT ?name WHERE { " + "?s r:type c:Line. " + "?s c:IdentifiedObject.name ?name} "
 				+ "ORDER by ?name";
 		ResultSet results = queryHandler.query(lineQuery);
@@ -48,24 +58,15 @@ public class TestManagerQueryFactory {
 		return subGeoName;
 	}
 	
-	public QueryHandler getQueryHandler() {
-		// // list all the connectivity nodes (aka bus)
-		// PREFIX r: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-		// PREFIX c: <http://iec.ch/TC57/2012/CIM-schema-cim16#>
-		// SELECT ?name WHERE {
-		// ?s r:type c:Line.
-		// ?s c:IdentifiedObject.name ?name}
-		// ORDER by ?name
-//		String szQuery = "		PREFIX r:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
-//				+ "PREFIX c:  <http://iec.ch/TC57/2012/CIM-schema-cim16#> " + "SELECT ?name WHERE { "
-//				+ "?s r:type c:Line. " + "?s c:IdentifiedObject.name ?name} " + "ORDER by ?name";
+	public QueryHandler getQueryHandler() {		
+		String bgHost = configurationManager.getConfigurationProperty(GridAppsDConstants.BLAZEGRAPH_HOST_PATH);
+		if(bgHost==null || bgHost.trim().length()==0){
+			bgHost = "http://localhost:9999/blazegraph";
+//			bgHost = "http://blazegraph:8080/bigdata";
+		}
 
-//		String bgHost = configurationManager.getConfigurationProperty(GridAppsDConstants.BLAZEGRAPH_HOST_PATH);
-//		if (bgHost == null || bgHost.trim().length() == 0) {
-//			bgHost = "http://localhost:9999";
-//		}
-		String bgHost = "http://localhost:9999";
-		QueryHandler queryHandler = new BlazegraphQueryHandler(bgHost+"/blazegraph/namespace/kb/sparql");
+//		System.out.println("Blazegraph "+ bgHost);
+		QueryHandler queryHandler = new BlazegraphQueryHandler(bgHost+"/namespace/kb/sparql");
 		return queryHandler;
 	}
 	
@@ -74,7 +75,7 @@ public class TestManagerQueryFactory {
 		if (results.hasNext()) {
 			QuerySolution soln = results.next();
 			feederName = soln.get("?name").toString();
-			System.out.println(feederName);
+//			System.out.println(feederName);
 		}
 		return feederName;
 	}
