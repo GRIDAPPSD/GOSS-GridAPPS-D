0600f3400daded8de9daab138f326f6942d518ab
Tonya Martin <tonya.martin@pnnl.gov>
2018-04-26 14:20:59 -0700

    rename run-docker.sh run-gridappsd.sh, added version option and log removal

---
+++ run-gridappsd.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo " "
+if [ "$1" = "version" -o "$1" = "-v" -o "$1" = "--version" ]; then
+  if [ -f /gridappsd/dockerbuildversion.txt ]; then
+    echo -n "version: "
+    cat /gridappsd/dockerbuildversion.txt
+  else
+    echo "Error: can't find version"
+  fi
+  echo " "
+  exit 0
+fi
+
+# Setup the path for running the gridappsd framework
+export PATH=/gridappsd/services/fncsgossbridge/service:$PATH
+
+cd /gridappsd
+
+# clean up log files
+if [ -d /gridappsd/log ]; then
+  /bin/rm -rf /gridappsd/log/* 2 > /dev/null
+fi
+
+# If the DEBUG environmental variable is set and is not 0
+# then expose the port for remote debugging.
+if [ "${DEBUG:-0}" != "0" ]; then
+	java -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n -jar lib/run.bnd.jar 
+else
+	java -jar lib/run.bnd.jar
+fi
