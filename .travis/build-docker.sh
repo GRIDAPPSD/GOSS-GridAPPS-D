a4366c968af8f165fcdb0dcbaf6e2edba824468d
Tonya Martin <tonya.martin@pnnl.gov>
2018-09-06 15:08:55 -0700

    Update travis builds, applications dir, gridappsd user setup

--- .travis/build-docker.sh
+++ .travis/build-docker.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-usage () {
-  /bin/echo "Usage:  $0 -b Build the docker image"
-  /bin/echo "           -p Push image to dockerhub"
-  exit 2
-}
-
 TAG="$TRAVIS_BRANCH"
 
 ORG=`echo $DOCKER_PROJECT | tr '[:upper:]' '[:lower:]'`
@@ -17,26 +11,60 @@ GITHASH=`git log -1 --pretty=format:"%h"`
 BUILD_VERSION="${TIMESTAMP}_${GITHASH}${TRAVIS_BRANCH:+:$TRAVIS_BRANCH}"
 echo "BUILD_VERSION $BUILD_VERSION"
 
-# parse options
-while getopts bp option ; do
-  case $option in
-    b) # Pass gridappsd tag to docker-compose
-      # Docker file on travis relative from root.
-      docker build --build-arg TIMESTAMP="${BUILD_VERSION}" -t ${IMAGE}:${TIMESTAMP}_${GITHASH} .
-      ;;
-    p) # Pass gridappsd tag to docker-compose
-      if [ -n "$TAG" -a -n "$ORG" ]; then
-        echo "docker push ${IMAGE}:${TIMESTAMP}_${GITHASH}"
-        docker push ${IMAGE}:${TIMESTAMP}_${GITHASH}
-        docker tag ${IMAGE}:${TIMESTAMP}_${GITHASH} ${IMAGE}:$TAG
-        echo "docker push ${IMAGE}:$TAG"
-        docker push ${IMAGE}:$TAG
-      fi
-      ;;
-    *) # Print Usage
-      usage
-      ;;
-  esac
-done
-shift `expr $OPTIND - 1`
+# Pass gridappsd tag to docker-compose
+docker build --build-arg TIMESTAMP="${BUILD_VERSION}" -t ${IMAGE}:${TIMESTAMP}_${GITHASH} .
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Error: status $status"
+  exit 1
+fi
 
+# To have `DOCKER_USERNAME` and `DOCKER_PASSWORD`
+# filled you need to either use `travis`' cli
+# (https://github.com/travis-ci/travis.rb)
+# and then `travis set ..` or go to the travis
+# page of your repository and then change the
+# environment in the settings pannel.
+
+if [ -n "$DOCKER_USERNAME" -a -n "$DOCKER_PASSWORD" ]; then
+
+  echo " "
+  echo "Connecting to docker"
+
+  echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+  status=$?
+  if [ $status -ne 0 ]; then
+    echo "Error: status $status"
+    exit 1
+  fi
+
+  if [ -n "$TAG" -a -n "$ORG" ]; then
+    # Get the built container name, for builds that cross the hour boundary
+    CONTAINER=`docker images --format "{{.Repository}}:{{.Tag}}" ${IMAGE}`
+
+    echo "docker push ${CONTAINER}"
+    docker push "${CONTAINER}"
+    status=$?
+    if [ $status -ne 0 ]; then
+      echo "Error: status $status"
+      exit 1
+    fi
+
+    echo "docker tag ${CONTAINER} ${IMAGE}:$TAG"
+    docker tag ${CONTAINER} ${IMAGE}:$TAG
+    status=$?
+    if [ $status -ne 0 ]; then
+      echo "Error: status $status"
+      exit 1
+    fi
+
+    echo "docker push ${IMAGE}:$TAG"
+    docker push ${IMAGE}:$TAG
+    status=$?
+    if [ $status -ne 0 ]; then
+      echo "Error: status $status"
+      exit 1
+    fi
+  fi
+
+fi
